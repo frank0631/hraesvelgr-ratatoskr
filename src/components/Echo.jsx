7c73851aaf16cb8d53eb2a844a077f92ee4d9322
Frank Rodriguez <frank0631@gmail.com>
2018-08-09 16:54:58 +0000

    remove old commented out code

--- src/components/Echo.jsx
+++ src/components/Echo.jsx
@@ -2,26 +2,23 @@ import React, {Component} from 'react';
 import { Text, TextInput, View, Button } from 'react-native';
 import $ from 'jquery'
 
-//import nidhogg from 'nidhogg-jquery';
 var echo_service = require('nidhogg-jquery/gen-js/TEchoService.js') 
-
-//import Nidhogg from 'nidhogg-jquery';
 
 class Echo extends Component {
 
   constructor(props) {
     super(props);
     this.state = { text: 'echo this text' , echo: 'echoed'};
-//    this.testThrift = this.testThrift.bind(this);
   }
 
- testThrift(text) {
+ echo() {
  	var transport = new Thrift.TXHRTransport("http://hraesvelgr.frank0631.com:9000/thrift/echo");
     var protocol  = new Thrift.TJSONProtocol(transport);
     var client    = new TEchoServiceClient(protocol);
     
     client.echo(this.state.text, function(result) {
     	this.setState({echo : result});
+    	transport.close();
     	}.bind(this));
   }
 
@@ -35,7 +32,7 @@ class Echo extends Component {
 	        value={this.state.text} />
 	        
         <Button style={{padding: 10, fontSize: 42}}
-        	title="test echo" onPress={() => this.testThrift(this.state.text)} />
+        	title="test echo" onPress={() => this.echo()} />
         
         <Text style={{padding: 10, fontSize: 42}}>
         	{this.state.echo}
