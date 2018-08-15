5966a23dc60e09e1fa41a7c5bf6a16438bcf52db
Frank Rodriguez <frank0631@gmail.com>
2018-08-15 15:32:17 +0000

    list customers as a result from rest request

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,11 +1,50 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 
+var hybind = require("hybind");
+
 class Customers extends Component {
+	
+  constructor(props) {
+    super(props);
+    this.state = {
+    	loading: true,
+    	customers: [] 
+    	};
+    }
+    
+	componentDidMount() {
+		var customers = [];
+		var api = hybind("http://hraesvelgr.frank0631.com:9000/data");
+		api.$bind("customers", customers);
+		
+		customers.$load().then(function() {
+		  console.log(customers.length);
+		  this.setState({
+	          loading: false,
+	          customers: customers,
+	        });
+		}.bind(this));
+	}
+	
   render() {
+	if(this.state.loading){
+      return(
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      )
+    };
+	
+	var customerList = 
+		this.state.customers.map(c => ( 
+		<Text key={c.id} >{c.firstName} {c.lastName}</Text>
+		))    
+	
     return (
       <View >
         <Text>Customers</Text>
+        {customerList}
       </View>
     );
   }
