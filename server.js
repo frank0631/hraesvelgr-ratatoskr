0d378dd7ada2e475716ee7c9d7b999abb0b5c856
Frank Rodriguez <frank0631@gmail.com>
2018-09-01 00:29:10 +0000

    add production setup

---
+++ server.js
@@ -0,0 +1,18 @@
+var path = require("path"),
+    express = require("express");
+
+var DIST_DIR = path.join(__dirname, "dist"),
+	PUBLIC_DIR = path.join(__dirname, "public"),
+    PORT = 9000,
+    app = express();
+
+//Serving the files on the dist & public folder
+app.use("/dist",express.static(DIST_DIR));
+app.use(express.static(PUBLIC_DIR));
+
+//Send index.html when the user access the web
+app.get("*", function (req, res) {
+  res.sendFile(path.join(PUBLIC_DIR, "index.html"));
+});
+
+app.listen(PORT);
---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: ['./src/index.web.jsx'],
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+        	presets: ['env'],
+      	},
+      },
+      {
+      	test: /\.js$/,
+	    include: [
+	        /node_modules\/nidhogg-jquery/
+	    ]
+      },
+	  {
+	    // Most react-native libraries include uncompiled ES6 JS.
+	    test: /\.js$/,
+	    include: [
+	        /node_modules\/react-native-/,
+	        /node_modules\/react-router-native/,
+	        /node_modules\/@indec/
+	    ],
+	    loader: 'babel-loader',
+	    query: {
+	        cacheDirectory: '.babel-cache'
+	    }
+	  },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader']
+      }
+    ]
+  },
+    resolve: {
+        alias: {
+            "react-native$": "react-native-web"
+        },
+        extensions: ['*', '.jsx',  '.web.js', '.js' ]    
+     },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  }
+};
