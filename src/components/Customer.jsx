106551f1e2652fd5310ffb6cf812b95ad20d16f3
Frank Rodriguez <frank0631@gmail.com>
2018-10-08 13:11:25 +0000

    pass props for single customer component/ displat first and last name

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 
 class Customer extends Component {
+
+	constructor(props) {
+		super(props);
+    }
+
   render() {
+  	var c = this.props.customer;
+  	
     return (
       <View >
-        <Text>Customer</Text>
+        <Text style={{padding: 10, fontSize: 42}} >{c.firstName} {c.lastName}</Text>
       </View>
     );
   }
